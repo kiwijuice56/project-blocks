f53e2c1ceebebe9e27e9f92519de371573cffcb3
kiwijuice56 <alfaroeric127@gmail.com>
2024-09-27 23:47:31 -0400

    Basic player

---
+++ game/main/player.gd
@@ -0,0 +1,26 @@
+class_name Player extends CharacterBody3D
+
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+var speed = 5
+var jump_speed = 5
+var mouse_sensitivity = 0.002
+
+func _ready() -> void:
+	pass
+
+func _physics_process(delta):
+	velocity.y += -gravity * delta
+	var input = Input.get_vector("left", "right", "up", "down")
+	var movement_dir = transform.basis * Vector3(input.x, 0, input.y)
+	velocity.x = movement_dir.x * speed
+	velocity.z = movement_dir.z * speed
+
+	move_and_slide()
+	if is_on_floor() and Input.is_action_just_pressed("jump"):
+		velocity.y = jump_speed
+
+func _input(event):
+	if event is InputEventMouseMotion and Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
+		rotate_y(-event.relative.x * mouse_sensitivity)
+		$Camera3D.rotate_x(-event.relative.y * mouse_sensitivity)
+		$Camera3D.rotation.x = clampf($Camera3D.rotation.x, -deg_to_rad(70), deg_to_rad(70))
--- game/main/test_world.gd
+++ game/main/test_world.gd
@@ -1,11 +1,15 @@
 class_name TestWorld extends World
 
 func _ready() -> void:
-	pass
+	for i in range(-3, 4):
+		for j in range(3, 4):
+			center = Vector3(i, 0, j) * 16
+	center = Vector3()
+	generate_from_queue(100)
 
 var x: int = 0
 
 func _physics_process(_delta: float) -> void:
-	center = %TestCamera.position
+	center = %Player.position
 	generate_from_queue(x % 2)
 	x += 1
